eaaff45d4f026fa139792ff0dd056df3917a0b6b
dan-laskowski <daniel.laskowski94@gmail.com>
2023-11-20 11:00:21 +0100

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { LoaderComponent } from './components/loader/loader.component';
+import { LoaderState } from './components/loader/loader.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, LoaderComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = '9-loader';
+export class AppComponent implements OnInit {
+  loaderState: LoaderState = LoaderState.isLoading;
+  spinnerText: string = 'konfiguracja narzędzi...';
+  spinnerIcon: string = '../assets/gear.svg';
+
+  public LOADER_STATE = LoaderState;
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.loaderState = LoaderState.isReady;
+    }, 6000);
+  }
 }
